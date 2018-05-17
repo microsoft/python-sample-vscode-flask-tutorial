721412d1bf05af030fce0156053eceb1bc8c2b89
Kraig Brockschmidt <kraigbr@msn.com>
2018-05-17 10:30:47 -0700

    Allow flask 1.x, match runserver.py code to tutorial

--- runserver.py
+++ runserver.py
@@ -1,11 +1,11 @@
-import os
+from os import environ
 from HelloFlask import app
 
 if __name__ == '__main__':
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    HOST = environ.get('SERVER_HOST', 'localhost')
 
     try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
     
