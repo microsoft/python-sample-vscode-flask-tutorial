9f4dd5708ec31a40bc1f596a7ddf8ec6746f5c2d
Kraig Brockschmidt <kraigb@microsoft.com>
2018-07-11 10:07:31 -0700

    Tweaks from Adrian's review

--- hello_flask/views.py
+++ hello_flask/views.py
@@ -1,10 +1,6 @@
 from datetime import datetime
-from re import match
-
 from flask import Flask, render_template
-
-from HelloFlask import app
-
+from hello_flask import app
 
 @app.route('/')
 def home():
@@ -22,23 +18,11 @@ def contact():
 def hello_there(name):
     return render_template(
         "hello_there.html",
-        title ='Hello, Flask',
-        name = clean_name(name),
-        date = datetime.now()
+        title='Hello, Flask',
+        name=name,
+        date=datetime.now()
     )
 
 @app.route('/api/data')
 def get_data():
     return app.send_static_file('data.json')
-
-def clean_name(name):
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    return clean_name
