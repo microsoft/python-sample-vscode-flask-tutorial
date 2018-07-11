89bd6398bc7fe8f100f86670acd5ce948f014274
Brett Cannon <brcan@microsoft.com>
2018-07-11 16:42:58 -0700

    Comment as to why imports are being done

--- hello_app/app.py
+++ hello_app/app.py
@@ -1,5 +1,5 @@
-from hello_app import app
-from hello_app import views
+from hello_app import app  # For application discovery by the 'flask' command.
+from hello_app import views  # For import side-effects of setting up routes.
 
 # Time-saver: output a URL to the VS Code terminal so you can easily Ctrl+click to open a browser
 # print('http://127.0.0.1:5000/hello/VSCode')
