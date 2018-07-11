15f9b52e151e7e103625252ee0a9172923f1910f
Kraig Brockschmidt <kraigb@microsoft.com>
2018-07-11 11:00:15 -0700

    Mode page title to content block

--- hello_app/views.py
+++ hello_app/views.py
@@ -4,15 +4,15 @@ from hello_app import app
 
 @app.route('/')
 def home():
-    return render_template("home.html", title="Home")
+    return render_template("home.html")
 
 @app.route('/about')
 def about():
-    return render_template("about.html", title="About us")
+    return render_template("about.html")
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html", title="Contact us")
+    return render_template("contact.html")
 
 @app.route('/hello/<name>')
 def hello_there(name):
