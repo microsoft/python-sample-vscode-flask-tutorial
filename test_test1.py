8e2b13fdb8020718c3099df31f6a7facb04f612f
Santiago Gonzalez <santiagosk80@gmail.com>
2020-10-23 07:25:15 -0500

    ac

--- test_test1.py
+++ test_test1.py
@@ -1,11 +1,14 @@
 import pytest
 
+def capital_case(x):
+    return x.capitalize()
+    
 def test_capital_case():
     assert capital_case('semaphore') == 'Semaphore'
 
 def test_raises_exception_on_non_string_arguments():
     with pytest.raises(TypeError):
         capital_case(9)
-        
+
 def test_mock():
   assert True
