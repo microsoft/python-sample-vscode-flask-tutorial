0bc9c0348a4a780ebf03041c5e37c756fe1545fd
Santiago Gonzalez <santiagosk80@gmail.com>
2020-10-26 10:02:04 -0500

    act

--- startup.py
+++ startup.py
@@ -1,9 +1,31 @@
-"""
-Code to add two numbers
-"""
+# -*- coding: utf-8 -*-
 
-a = 23
-b = 45
-c = a + b
 
-print(c)
+
+#Se importa numpy como np
+
+import numpy as np
+
+
+
+from pudb import set_trace; set_trace()
+
+import string
+
+
+
+a = np.arange(10)
+
+print a
+
+print a[8]
+
+print a[9]
+
+
+
+for i in a:
+
+    if i == plataforma:
+
+        print i
