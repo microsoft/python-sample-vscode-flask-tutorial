6f614fb1a21c2951bac21a6c43dd99c5af6e0d68
Santiago Gonzalez <santiagosk80@gmail.com>
2020-10-26 09:36:04 -0500

    act

--- startup.py
+++ startup.py
@@ -10,3 +10,8 @@ startup:app in the Gunicorn command.
 """
 
 from hello_app.webapp import app
+
+pycon = {2016: "Portland", 2018: "Cleveland"}
+europython = {2017: "Rimini", 2018: "Edinburgh", 2019: "Basel"}
+ 
+print({**pycon, **europython})
