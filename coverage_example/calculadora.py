8de5e99a352281da0361085161774de7cd582ab7
Santiago Gonzalez <santiagosk80@gmail.com>
2020-10-26 14:48:02 -0500

    act

---
+++ coverage_example/calculadora.py
@@ -0,0 +1,6 @@
+def add(first_term, second_term):
+    return first_term + second_term
+
+
+def subtract(first_term, second_term):
+    return first_term - second_term
---
+++ coverage_example/test_calculator.py
@@ -0,0 +1,14 @@
+"""
+Unit tests for the calculator library
+"""
+
+import calculator
+
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == calculator.add(2, 2)
+
+    def test_subtraction(self):
+        assert 2 == calculator.subtract(4, 2)
