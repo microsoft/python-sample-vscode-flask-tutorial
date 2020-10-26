de16426107f65b70fc0aca1c1b297d7e9b50680b
Santiago Gonzalez <santiagosk80@gmail.com>
2020-10-26 13:45:26 -0500

    act

--- hello_app/lambda.py
+++ hello_app/lambda.py
@@ -1,5 +1,5 @@
 def my_handler(event, context):
-    message = 'Hello {} {}!'.format(event['first_name'],event['last_name'])
+    message = 'Hello {} {}!'.format(event['first_name'], event['last_name'])
     return{
         'message': message
     }
