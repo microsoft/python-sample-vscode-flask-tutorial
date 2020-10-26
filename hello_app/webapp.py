7e198bc325bcfb80bb9e0290850df587926983cc
Santiago Gonzalez <santiagosk80@gmail.com>
2020-10-26 10:55:17 -0500

    ct

--- hello_app/webapp.py
+++ hello_app/webapp.py
@@ -1,3 +1,3 @@
 # Entry point for the application.
-from . import app  # For application discovery by the 'flask' command.
-from . import views  # For import side-effects of setting up routes. 
+from . import app
+from . import views
