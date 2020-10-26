3b73f7bb49921065f0ee15d3b86538d9a6ff0f86
Santiago Gonzalez <santiagosk80@gmail.com>
2020-10-26 10:32:38 -0500

    act

--- startup.py
+++ startup.py
@@ -5,15 +5,16 @@
 #Se importa numpy como np
 
 import numpy as np
-from pudb import set_trace; set_trace()
+from pudb import set_trace; 
+
+set_trace()
 
 a = np.arange(10)
 
-print(a) # silence pyflakes
+print(a)  # silence pyflakes
 print(a[8])
 print(a[9])
 plataforma = 2
 for i in a:
-    if i == plataforma: # silence pyflakes
+    if i == plataforma:  # silence pyflakes
         print(i)
-        
