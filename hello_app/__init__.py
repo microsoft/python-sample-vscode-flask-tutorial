1f05787ec0e32ae699fa3b3647456ef9f9c740eb
Bijoy <bijoy2942@outlook.com>
2020-11-08 23:17:31 +0530

    updated the init.py

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -1,2 +1,6 @@
 from flask import Flask  # Import the Flask class
-app = Flask(__name__)    # Create an instance of the class for our use
+app = Flask(__name__)    
+
+@app.route('/')
+def greet():
+    print("Hello World")
