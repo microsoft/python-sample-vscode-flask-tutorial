69e305153c0db7abc741fcaef9cd9d707a64a94d
David Martin <dmartin@xtreme-eda.com>
2021-02-24 14:45:11 -0500

    Add comment

--- startup.py
+++ startup.py
@@ -7,6 +7,8 @@ file through Gunicorn; the result is "Attempted relative import in non-package".
 The solution is to provide a simple alternate startup file, like this present
 startup.py, that just imports the app object. You can then just specify
 startup:app in the Gunicorn command.
+...
+Add some more comments
 """
 
 from hello_app.webapp import app
