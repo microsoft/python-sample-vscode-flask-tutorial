5406569ff6020f76e436ccb44ff585312c708338
Johnathon Hyland <johnathon.hyland@ligo.org>
2022-07-07 11:51:17 +0100

    updates to about

--- app.py
+++ app.py
@@ -6,14 +6,17 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello_there(name = None):
-    return render_template(
-        "hello_there.html",
-        name=name,
-        date=datetime.now()
-    )
+def home():
+    return render_template("home.html")
+
+# New functions
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
 
 @app.route("/api/data")
 def get_data():
