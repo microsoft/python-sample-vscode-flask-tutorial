959e2077b5c453507c9c023647f39ba83f2dc478
Johnathon Hyland <johnathon.hyland@ligo.org>
2022-07-07 10:11:43 +0100

    code snippets, new pages, static

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from datetime import datetime
+import re
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
